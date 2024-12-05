029392fd0c2b0367f27f13a72b56e9b647589f18
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-05 08:56:19 +0100

    Initial Commit

---
+++ SwiftUIProject6-Fructus/SwiftUIProject6_FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject6_FructusApp.swift
+//  SwiftUIProject6-Fructus
+//
+//  Created by Yaro Paul  on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject6_FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
